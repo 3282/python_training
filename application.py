fc762bebfd88c59dadfbed8a03a62f91f5cac503
Daria Fedorova <darya.fedorova.97@gmail.com>
2021-12-16 19:43:58 +0300

    Добавление в Application метода для создания контактов

--- application.py
+++ application.py
@@ -10,6 +10,64 @@ class Application:
         wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
+
+    def fill_new_contact_form(self, contact):
+        # fill new contact form
+        wd = self.wd
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
+        wd.find_element_by_name("fax").click()
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(contact.fax)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("email2").click()
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(contact.email2)
+        wd.find_element_by_name("email3").click()
+        wd.find_element_by_name("email3").clear()
+        wd.find_element_by_name("email3").send_keys(contact.email3)
+        wd.find_element_by_name("bday").click()
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
+        wd.find_element_by_xpath("//option[@value='17']").click()
+        wd.find_element_by_name("bmonth").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
+        wd.find_element_by_xpath("//option[@value='December']").click()
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(contact.byear)
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(contact.address2)
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def open_contact_name(self):
+        # open contact page
+        wd = self.wd
+        wd.find_element_by_link_text("add new").click()
     def return_to_groups_page(self):
         # return to groups page
         wd = self.wd
@@ -52,7 +110,7 @@ class Application:
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def open_home_page(self:
+    def open_home_page(self):
         # open home page
         wd = self.wd
         wd.get("http://localhost/addressbook/")
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 import pytest
 from contact import Contact
-from application_1 import Application_1
+from application import Application
 
 
 @pytest.fixture
-def app_1(request):
-    fixture = Application_1()
+def app(request):
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
 
-def test_add_contact(app_1):
-        app_1.open_home_page()
-        app_1.login(username="admin", password="secret")
-        app_1.open_contact_name()
-        app_1.fill_new_contact_form(Contact(firstname="Maria", middlename="Olegovna", lastname="Bakholdina", nickname="Masha", company="YOTA", address="Yakhtennaya 6", home="6789688",
+def test_add_contact(app):
+        app.open_home_page()
+        app.login(username="admin", password="secret")
+        app.open_contact_name()
+        app.fill_new_contact_form(Contact(firstname="Maria", middlename="Olegovna", lastname="Bakholdina", nickname="Masha", company="YOTA", address="Yakhtennaya 6", home="6789688",
                                    mobile="89995567584", fax="896-888", email="test1@mail.ru", email2="test2@mail.ru", email3="test3@mail.ru", bday="17",
                                    bmonth="December", byear="1973", address2="Antonova-Ovseenko 13"))
-        app_1.logout()
+        app.logout()
 
