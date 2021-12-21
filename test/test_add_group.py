b4d5f1822a214ab323358daf3601d3a24c3faf35
Daria Fedorova <darya.fedorova.97@gmail.com>
2021-12-21 12:51:35 +0300

    Созданы пакеты для разных зон ответственности

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from group import Group
-from application import Application
+from model.group import Group
+from fixture.application import Application
 
 
 @pytest.fixture
@@ -12,7 +12,7 @@ def app(request):
 
 
 def test_add_group(app):
-        app.login( username="admin", password="secret")
+        app.login(username="admin", password="secret")
         app.create_group(Group(name="dfgdfg", header="dfgdfg", footer="dfgdfgdfg"))
         app.logout()
 
